b80853e794701553e87fed4e15d6556e20cb9231
Oleg Muraviov <95213060+OlegMuraviov@users.noreply.github.com>
2022-06-21 10:30:57 +0300

    update names

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import './index.css';
 const basename = process.env.REACT_APP_BASE_NAME;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* basename="/react-team-project/" */}
+    {/* basename="/react-pro-test/" */}
     <BrowserRouter basename={basename}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
